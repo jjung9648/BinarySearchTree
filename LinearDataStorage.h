55fce78d04c2b95c8c6097303f47f5843b6bc015
jjung9648 <jjung1469@gmail.com>
2025-06-22 23:03:13 -0700

    add linear data structure for run time estimation

---
+++ LinearDataStorage.h
@@ -0,0 +1,134 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Node//linear tree will be consist with Node
+{
+public:
+    Node *apRight;//right child
+    int aKey;//The key to find the value.
+    string aValue;//The value that the node contains.
+    Node(int rKey, string rValue)//constructor
+    {
+        aKey = rKey;
+        aValue = rValue;
+    }
+    string ToTree()//Returns a string of the tree’s structure. 
+    {
+        string result = "";
+        result = result + " --> " + "(" + to_string(aKey) + ") " + aValue;//add label, key, and value of the node
+
+        if (apRight != nullptr)//if right child of current node exists, change current node to that and and rIndent+1
+        {
+            result += apRight->ToTree();
+        }
+        return result;
+    }
+    // string ToSortedList()//Returns a string sorted by the key.
+    // {
+    //     string result = "";
+    //     if (apLeft != nullptr)//if left child is not null, change current node to that
+    //     {
+    //         result += apLeft->ToSortedList();
+    //     }
+    //     result = result + "(" + to_string(aKey) + ") " + aValue + ", " ;//add key, value, and comma at string
+    //     if (apRight != nullptr)//if right child is not null, change current node to that
+    //     {
+    //         result += apRight->ToSortedList();
+    //     }
+    //     return result;
+    // }
+    void Add(int rKey, string rValue)//Adds the key to the correct position in the BST. If the key already exists, do nothing.
+    {
+        if (apRight == nullptr)//if right child of current node is null, set current node to right child
+        {
+            apRight = new Node(rKey, rValue);
+        }
+        else//if right child of current node is not null, set current node to right child
+        {
+            apRight->Add(rKey, rValue);
+        }
+    }
+    string Find(int rKey)//Finds a node with the key and returns its value. If the node is not found, you can return an empty string.
+    {
+        string result;
+        if (aKey == rKey)//if current aKey and rKey is same, result is the value of current node
+        {
+            result = aValue;
+        }
+        else
+        {
+            if (apRight == nullptr){
+                return "";
+            }
+            result = apRight->Find(rKey);
+        }
+        return result;//return result which is a value or empty string.
+    }
+};
+
+class BinarySearchTree//simply starts recursion of the root itself
+{
+private:
+    Node *apRoot;//root node of the binary tree
+public:
+    BinarySearchTree() : apRoot(nullptr) {}//Constructor
+    string About()//Returns text about you – the author of this class.
+    {
+        return "This program is made by Jaehee Jung in class 130";
+    }
+    void Add(int rKey, string rValue)//Adds the key to the correct position in the BST. If the key already exists, do nothing.
+    {
+        if (apRoot == nullptr)//if apRoot is null, it means added node will be the root. set apRoot to new node
+        {
+            apRoot = new Node(rKey, rValue);
+        }
+        else//if root node exists, start recursion from root node
+        {
+            apRoot->Add(rKey, rValue);
+        }
+    }
+    string ToTree()//Returns a string of the tree’s structure. It will start recursion from the root node with indent 0 and a label of "Root".
+    {
+        string result;
+        if (apRoot == nullptr)//if root node does not exists, it means there's nothing to print, result is empty space.
+        {
+            result = "";
+        }
+        else//if root node exists,start recursion from root node
+        {
+            result = apRoot->ToTree();
+        }
+        return result;
+    }
+
+    // string ToSortedList()//Returns a string sorted by the key.
+    // {
+    //     string result;
+    //     if (apRoot == nullptr)//if root node does not exists, it means there's nothing to print, result is empty space.
+    //     {
+    //         result = "";
+    //     }
+    //     else//if root node exists,start recursion from root node
+    //     {
+    //         result = apRoot->ToSortedList();
+    //         result.pop_back();//pop back space and comma at the end of the string
+    //         result.pop_back();
+    //     }
+    //     return result;
+    // }
+
+    string Find(int rKey)//Finds a node with the key and returns the value. If the node is not found, you can return an empty string.
+    {
+        string result;
+        if (apRoot == nullptr)//if no root node, nothing in the tree. result is empty string
+        {
+            result = "";
+        }
+        else//if root node exists,start recursion from root node
+        {
+            result = apRoot->Find(rKey);
+        }
+        return result;
+    }
+};
---
+++ main_linear.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "LinearDataStorage.h"
+using namespace std;
+
+int main()//Test two classes in BinarySearchTree.h
+{
+    int key;//get key from the file
+    string s;//get value from the file
+    string files[7] = {"areacode.txt", "pokemon.txt", "california.txt", "stooges.txt", "halloween calories 2.txt", "years.txt", "halloween calories.txt" };
+
+    // only for test.
+    int testArr[7] = {510, 1, 10, 3, 200, 1945, 120};
+
+    for(int i = 0; i < 7; i++)//make results of all 7 files in files array with for loop
+    {
+        BinarySearchTree b;
+        ofstream output(to_string(i)+"th_testfile_linear.txt");//make output file to write result. Name will be ith_testfile.txt
+        output << b.About() << endl;//test About() method
+        output << files[i] << "(Empty tree test):" << endl << b.ToTree() << endl << endl;//testing the case tree is empty. No ascii tree will be made
+        // output << b.ToSortedList() << endl << endl << endl;//testing the case tree is empty. No sorted tree will be made
+        cout << "what do you want to find at " + files[i] + "?(Result must empty): ";//testing the case tree is empty, nothing will be found
+        output << "what do you want to find at " + files[i] + "?(Result must empty): ";//printing the result at both terminal and output file
+        // int number;
+        // cin >> number;
+        // for test
+        int number = testArr[i];
+        output << number << endl;
+        string found = b.Find(number);//test Find() method. Get the result at found string(no result)
+        cout << found << endl;
+        output << found << endl;
+        ifstream input(files[i]);//open the input file to add the contents of tree
+        input >> key;//get key from the file
+        getline(input,s);//remove the empty space
+        getline(input,s);//get value from the file
+        while (isspace(s.back()))//remove space at the end of the string of s
+        {
+            s.pop_back();
+        }
+        while (s != "END")//read the file and get key and values until value got is END
+        {
+            b.Add(key, s);//add method() test
+            input >> key;//get key from the file
+            getline(input,s);//remove the empty space
+            getline(input,s);//get value from the file
+            while (isspace(s.back()))//remove space at the end of the string of s
+            {
+                s.pop_back();
+            }
+        }
+        input.close();//close the input file
+        output << files[i] << ":" << endl << b.ToTree() << endl << endl;//test ToTree() method and write result at the output file
+        // output << b.ToSortedList() << endl << endl << endl;//test ToSortedList() method and write result at the output file
+        cout << "what do you want to find at " + files[i] + "?(type key): ";
+        output << "what do you want to find at " + files[i] + "?(type key): ";
+        // cin >> number;
+        output << number << endl;
+        found = b.Find(number);//test Find() method and write result at the output file and terminal
+        cout << found << endl;
+        output << found << endl;
+        output.close();//close the output file
+    }
+}
